3d3d2b68de1a3a75aab384061a9a666579f0db4c
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2023-07-19 21:35:49 +0300

    add store and persist

---
+++ src/redux/store.js
@@ -0,0 +1,36 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import persistReducer from 'redux-persist/es/persistReducer';
+
+import { productsReducer } from './products/slice';
+import { filterSlice } from './filter/slice';
+
+const productsPersistConfig = {
+  key: 'products',
+  storage,
+};
+
+export const store = configureStore({
+  reducer: {
+    products: persistReducer(productsPersistConfig, productsReducer),
+    filter: filterSlice.reducer,
+  },
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
+});
+
+export const persistor = persistStore(store);
