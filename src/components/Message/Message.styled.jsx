ec907aa19a1227eb2ed44f8591f1d5306d30d6d4
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2023-07-26 00:03:32 +0300

    add Message component

---
+++ src/components/Message/Message.styled.jsx
@@ -0,0 +1,10 @@
+import { styled } from 'styled-components';
+
+export const MessageBox = styled.div`
+  height: 30vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px;
+  color: #746e6e;
+`;
