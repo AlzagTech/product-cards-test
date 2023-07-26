a97c9e39ec61e2331f424d186d0c40f93116dd44
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2023-07-26 09:52:48 +0300

    Update Filter.styled.jsx

--- src/components/Filter/Filter.styled.jsx
+++ src/components/Filter/Filter.styled.jsx
@@ -89,32 +89,14 @@ export const CategoryBtn = styled.button`
 
 export const CategoryBox = styled.div`
   display: none;
-  position: absolute;
   overflow: hidden;
-  /* height: 0%; */
-  /* 
+
   &.isOpen {
     display: block;
   }
-
-  &.isOpen ul {
-    transform: translateY(0%);
-  } */
-
-  &.isOpen {
-    position: relative;
-    display: block;
-    transition: transform 2000ms ease-in-out;
-    transform: translateY(0%);
-  }
-
-  transform: translateY(-110%);
 `;
 
 export const CategoryList = styled.ul`
-  /* transition: transform 2000ms ease-in-out;
-  transform: translateY(-110%); */
-
   font-size: 0;
   display: flex;
   flex-wrap: wrap;
